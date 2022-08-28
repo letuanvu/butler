33ecd7263132df8e562dd68d64ddb3e44b321710
vu.letuan <ltvu.59@gmail.com>
2022-08-28 14:41:30 +0700

    connect db

--- Http.js
+++ Http.js
@@ -1,11 +1,30 @@
 const express = require('express');
-const index = require('./routes/index')
+const mongoose = require('mongoose');
+
+/* routes include */
+const indexPage = require('./routes/indexPage');
+const loginPage = require('./routes/loginPage');
 
 class Http {
 	constructor() {
 		this.express = express();
 		this.express.set('view engine', 'ejs');
-		this.express.use('/', index);
+		this.moutRoutes();
+		Http.connectMongo();
+	}
+
+	static connectMongo() {
+		try {
+			mongoose.connect(process.env.MONGO_URI);
+			console.log("Connected to Mongodb")			
+		} catch (connectError) {
+			console.error(connectError);
+		}
+	}
+
+	moutRoutes() {
+		this.express.use(indexPage);
+		this.express.use(loginPage);
 	}
 
 	getApp() {
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const Http = require('./Http');
 const PORT = process.env.PORT || 80;
-
+require('dotenv').config();
 
 new Http().getApp().listen(PORT, () => {
 	console.log(`bulter listening on port ${PORT}`);
